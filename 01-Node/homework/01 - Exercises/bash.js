82b80b39c6af6139f4a2379f191e04dd0a58a1a2
Edeok <113571732+Edeok@users.noreply.github.com>
2023-01-13 18:41:43 -0300

    ejercicio 1

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,7 +1,29 @@
 const process = require("process");
 const commands = require("./commands/index.js");
 
+
+function print (output){
+  process.stdout.write(output)
+  process.stdout.write("\nprompt > ")
+
+
+
+}
+
 function bash() {
+  process.stdout.write( "prompt > ")
+  process.stdin.on('data', (data)=>{
+    var args = data.toString()
+
+    var cmd =  args
+
+    if (!commands === cdm) {
+      print()
+      console.log('command not found: _cmd_')
+    }else{
+      commands[cmd](print, args);
+    }
+  })
 }
 
 bash();
